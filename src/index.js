c7ba22d20e13c2b8279c8b48daeee3e910033f4a
Aja Washington <aja.washington13@gmail.com>
2021-01-04 18:29:00 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from 'react-apollo';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import client from './apollo';
 import reportWebVitals from './reportWebVitals';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <ApolloProvider client={client}>
+   <App />
+ </ApolloProvider>,  
+ document.getElementById('root')
 );
+registerServiceWorker.unregister();
+if (module.hot) module.hot.accept();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
